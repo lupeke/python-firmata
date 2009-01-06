b798edab51294f34eafbc40a0dde46b6adc23b24
Daniel Dias <lupeke@gmail.com>
2009-01-06 19:25:54 -0200

    Adding examples

---
+++ examples/led.py
@@ -0,0 +1,11 @@
+from firmata import *
+
+a = Arduino('/dev/tty.usbserial-A1001NQe')
+a.pin_mode(13, OUTPUT)
+a.delay(2)
+
+while True:
+    a.digital_write(13, HIGH)
+    a.delay(2)
+    a.digital_write(13, LOW)
+    a.delay()
